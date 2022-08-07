45ab35baaf4621eb1123257faf57fb4c6c16ffb9
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-07 19:29:41 -0400

    Update script.py

--- Len's Slice/script.py
+++ Len's Slice/script.py
@@ -8,3 +8,7 @@ prices = [2, 6, 1, 3, 2, 7, 2]
 #Your Boss wants you to do some research on $2 slices - Count the number of slices that cost $2 
 num_two_dollar_slices = prices.count(2)
 print(num_two_dollar_slices)
+
+#Find the length of the toppings list 
+num_pizzas = len(toppings)
+
