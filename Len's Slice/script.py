33902643fee6dac2fbda6ec6ffb29e1da4876427
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-07 19:28:55 -0400

    Update script.py

--- Len's Slice/script.py
+++ Len's Slice/script.py
@@ -7,3 +7,4 @@ prices = [2, 6, 1, 3, 2, 7, 2]
 
 #Your Boss wants you to do some research on $2 slices - Count the number of slices that cost $2 
 num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
