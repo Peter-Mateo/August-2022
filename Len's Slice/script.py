ddd6cbdfe27c67cab8cb65bc432e9d4e9d370b9b
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-07 19:27:43 -0400

    Update script.py

--- Len's Slice/script.py
+++ Len's Slice/script.py
@@ -1,2 +1,7 @@
-#Declaring list
+#Declaring list of toppings
 toppings = ['pepperoni', 'pineapple', 'cheese', 'sausage', 'olives', 'anchovies', 'mushrooms']
+
+
+#Declaring list of pizza slice costs
+prices = [2, 6, 1, 3, 2, 7, 2]
+
