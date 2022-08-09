32a89bce4cb8e7c32b6cb53341f453bff53fa8a5
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-08 22:20:58 -0400

    Update script.py

--- Len's Slice/script.py
+++ Len's Slice/script.py
@@ -7,13 +7,13 @@ prices = [2, 6, 1, 3, 2, 7, 2]
 
 #Your Boss wants you to do some research on $2 slices - Count the number of slices that cost $2 
 num_two_dollar_slices = prices.count(2)
-print(num_two_dollar_slices)
+#print(num_two_dollar_slices)
 
 #Find the length of the toppings list 
 num_pizzas = len(toppings)
 
 #5
-print(f"We sell {num_pizzas} different kinds of pizza!")
+#print(f"We sell {num_pizzas} different kinds of pizza!")
 
 #6 
 pizza_and_prices = [[2, 'pepperoni'], [6, 'pineapple'], [1, 'cheese'], [3, 'sausage'], [2, 'olives'], [7, 'anchovies'], [2, 'mushrooms']]
@@ -37,4 +37,11 @@ pizza_and_prices.insert(4, [2.5, 'peppers'])
 three_cheapest = pizza_and_prices[:3]
 
 #14
-print(three_cheapest)
+#print(three_cheapest)
+
+pizza_prices = []
+for i in range(len(toppings)):
+    add = [prices[i], toppings[i]]
+    pizza_prices.append(add)
+
+print(pizza_prices)
