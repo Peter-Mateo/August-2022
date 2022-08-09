d27a6b47444c308097408d6cb8d16c32cf62857c
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-08 23:16:18 -0400

    Update script.py

--- For Loops/script.py
+++ For Loops/script.py
@@ -1,6 +1,7 @@
 python_topics = ["variables", "control flow", "loops", "modules", "classes"]
 index = 0
+length = len(python_topics)
 
-while index < len(python_topics):
+while index < length:
     print(python_topics[index])
     index += 1
