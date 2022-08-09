43991bd1e8edb282d3affdd4c95b9535a1591deb
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-08 23:19:07 -0400

    Update script.py

--- For Loops/script.py
+++
@@ -1,7 +0,0 @@
-python_topics = ["variables", "control flow", "loops", "modules", "classes"]
-index = 0
-length = len(python_topics)
-
-while index < length:
-    print("I am learning about " + python_topics[index])
-    index += 1
