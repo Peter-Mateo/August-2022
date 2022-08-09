34d3a0eb32922822bed3a55b5f58b8fd0cd194b0
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-08 23:28:36 -0400

    Update script.py

--- For Loops/script.py
+++ For Loops/script.py
@@ -3,3 +3,5 @@ dog_breed_I_want = "dalmatian"
 
 for dog_breed in dog_breeds_available_for_adoption:
     print(dog_breed)
+    if dog_breed == dog_breed_I_want:
+        print("They have the dog I want")
