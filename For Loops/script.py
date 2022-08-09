b5112ba468cc806e3980b1082c1a877ca0afb334
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-08 23:04:39 -0400

    Update script.py

--- For Loops/script.py
+++ For Loops/script.py
@@ -1,9 +1,2 @@
-board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+promise = "I will finish the python loops module!"
 
-sport_games = ["football", "hockey", "baseball", "cricket"]
-
-for game in board_games:
-    print(game)
-
-for sport in sport_games:
-    print(sport)
