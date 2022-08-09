a79ef533720748d982586291fe14e2e82486959a
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-08 23:26:39 -0400

    Update script.py

--- For Loops/script.py
+++ For Loops/script.py
@@ -2,4 +2,4 @@ dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "
 dog_breed_I_want = "dalmatian"
 
 for dog_breed in dog_breeds_available_for_adoption:
-    
+    print(dog_breed)
