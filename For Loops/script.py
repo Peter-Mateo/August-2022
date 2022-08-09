cf164fd2a08d8d86acbf8b315f3aa755381bbea9
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-08 23:09:14 -0400

    Update script.py

--- For Loops/script.py
+++ For Loops/script.py
@@ -1,14 +1,14 @@
 count = 0
 print("Starting While Loop")
-while count <= 3:
-    # Loop Body
-    # Print if the condition is still true
-    print("Loop Iteration - count <= 3 is still true")
-    # Print the current value of count 
-    print("Count is currently " + str(count))
-    # Increment count
-    count += 1
-    print(" ----- ")
-print("While Loop ended")
+# while count <= 3:
+#     # Loop Body
+#     # Print if the condition is still true
+#     print("Loop Iteration - count <= 3 is still true")
+#     # Print the current value of count 
+#     print("Count is currently " + str(count))
+#     # Increment count
+#     count += 1
+#     print(" ----- ")
+# print("While Loop ended")
 
 
