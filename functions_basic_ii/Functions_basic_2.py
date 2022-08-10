bd8476cfec0da97721c78f8518fa9143876cd921
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-10 19:28:41 -0400

    Update Functions_basic_2.py

--- functions_basic_ii/Functions_basic_2.py
+++ functions_basic_ii/Functions_basic_2.py
@@ -9,7 +9,12 @@ print(countdown(5))
 
 #Create a function that takes a list and returns the sum of the first value in the list plus the list's length
 def sum(some_list):
+    print(some_list[0])
+    return some_list[1]
+num = [1,2]
+print(sum(num))
+
+#Create a function that accepts a list and returns the sum of the first value in the list plus the list's length
+def summer(some_list):
     a = some_list[0] + len(some_list)
     return a
-num = [1,2,3,4,5]
-print(sum(num))
