71bbb064ffc369221f31b12da97d18db14741096
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-10 19:30:35 -0400

    Update Functions_basic_2.py

--- functions_basic_ii/Functions_basic_2.py
+++ functions_basic_ii/Functions_basic_2.py
@@ -20,3 +20,5 @@ def summer(some_list):
     return a
 list = [1,2,3,4,5,6]
 print(summer(list))
+
+# Write a function that accpets a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return false. 
