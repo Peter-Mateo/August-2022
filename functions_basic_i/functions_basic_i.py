692b2502fb9e200ee80384d3798baf3ba5664f67
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-10 18:48:31 -0400

    Update functions_basic_i.py

--- functions_basic_i/functions_basic_i.py
+++ functions_basic_i/functions_basic_i.py
@@ -2,46 +2,47 @@
 def number_of_food_groups():
     return 5
 print(number_of_food_groups())
+# Prediction - (5)
 
 
 #2
 def number_of_military_branches():
     return 5
 print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
-
+# Prediction - ( Error number_of_days_in_a_week_silicon_or_triangle_sides not defined)
 
 #3
 def number_of_books_on_hold():
     return 5
     return 10
 print(number_of_books_on_hold())
-
+# Prediction - (5)
 
 #4
 def number_of_fingers():
     return 5
     print(10)
 print(number_of_fingers())
-
+# Prediction - (5)
 
 #5
 def number_of_great_lakes():
     print(5)
 x = number_of_great_lakes()
 print(x)
-
+# Prediction - (5)
 
 #6
 def add(b,c):
     print(b+c)
 print(add(1,2) + add(2,3))
-
+# Prediction - (8)
 
 #7
 def concatenate(b,c):
     return str(b)+str(c)
 print(concatenate(2,5))
-
+# Prediction - (25)
 
 #8
 def number_of_oceans_or_fingers_or_continents():
