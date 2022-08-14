ff92f6aafbfd9a32f51289cf168d4e83de7dfd32
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-14 08:49:46 -0400

    Update bank_account.py

---
+++ BankAccount/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance < amount:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        else:
+            self.balance -= amount
+
+    def display_info(self):
+        print(f"Balance: ${self.balance}")
+
+    def yield_interest(self):
+        self.balance = self.balance * .03
+
