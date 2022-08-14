8477dd9ff4a1bfc03675f7305e5456efd5be1b7d
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-14 12:16:44 -0400

    Update users_bankaccount.py

--- User_Bank/users_bankaccount.py
+++ User_Bank/users_bankaccount.py
@@ -1,7 +1,9 @@
+import random as rnd
 class BankAccount:
-    def __init__(self, int_rate, balance):
+    def __init__(self, int_rate, balance, account= None):
         self.int_rate = int_rate
         self.balance = balance
+        self.account_id = account
 
     @classmethod
     def instances(cls, int_rate, balance):
@@ -24,19 +26,23 @@ class BankAccount:
             return self
 
     def display_info(self):
-        print(f"Balance: ${self.balance}")
+        print(f"\n Balance: ${self.balance}\n Account: {self.account_id}")
         return self
 
     def yield_interest(self):
         self.balance = self.balance + round(self.balance * self.int_rate, 2)
         return self
 class User:
-    def __init__(self,name, email):
+    def __init__(self,name, email, account_id):
         self.name = name
         self.email = email
-        self.account = BankAccount(int_rate = 0.02, balance = 0)
+        self.account = BankAccount(int_rate = 0.02, balance = 0, account = account_id)
 
-    #Other methods
+    @classmethod 
+    def new_account(cls, int_rate = 0.02, balance = 0, account = None):
+        self.account = BankAccount(int_rate, balance, account)
+
+
     def make_deposit(self,amount):
         self.account.deposit(amount)
         return self
@@ -49,6 +55,9 @@ class User:
         self.account.display_info()
         return self
 
+user = User('Peter', 'empty@gmail.com', 305127)
+
+user.new_account('')
 # Allow a user to have multiple accounts; update methods so the user has to specify which account they are withdrawing or depositing to
 
-# Add a transfer_money(self,amount, other_user) method to the user class that takes an amount and a different User instance, and transfers money from the user's account into another user's account 
+# Add a transfer_money(self,amount, other_user) method to the user class that takes an amount and a different User instance, and transfers money from the user's account into another user's account
