bd4d7d349722418983476727e5fe6163b570961d
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-14 07:03:38 -0400

    Update user.py

--- User/user.py
+++ User/user.py
@@ -1,3 +1,6 @@
+from tkinter import N
+
+
 class User:
     def __init__(self, first_name, last_name, email, age, is_rewards_member=False, gold_card_points = 0):
         self.first_name = first_name
@@ -9,17 +12,19 @@ class User:
 
     def display_info(self):
         print(
-        self.first_name, 
-        self.last_name, 
-        self.email, 
-        self.age, 
-        self.is_rewards_member, 
-        self.gold_card_points
-        )
+        "\n", self.first_name, 
+        "\n", self.last_name, 
+        "\n", self.email, 
+        "\n", self.age, 
+        "\n", self.is_rewards_member, 
+        "\n", self.gold_card_points)
 
     def enroll(self):
-        self.is_rewards_member = True
-        self.gold_card_points = 200
+        if self.is_rewards_member == True:
+            self.is_rewards_member = True
+            self.gold_card_points = 200
+        else:
+            print("User already a member")
 
     def spend_points(self,amount):
         self.gold_card_points -= self.gold_card_points - amount
@@ -27,4 +32,7 @@ class User:
 ## Test ###
 student = User('Peter', 'Mateo', 'asp@gmail.com', 21)
 
-
+student.enroll()
+student.spend_points(100)
+student.display_info()
+student.enroll()
