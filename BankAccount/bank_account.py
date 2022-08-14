2a4005728a0fc28683879b17a09a0e60262fdadc
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-14 08:58:22 -0400

    Update bank_account.py

--- BankAccount/bank_account.py
+++ BankAccount/bank_account.py
@@ -6,19 +6,24 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += amount
+        return self
 
     def withdraw(self, amount):
         if self.balance < amount:
             print("Insufficient funds: Charging a $5 fee")
             self.balance -= 5
+            return self
         else:
             self.balance -= amount
+            return self
 
     def display_info(self):
         print(f"Balance: ${self.balance}")
+        return self
 
     def yield_interest(self):
         self.balance = self.balance + round(self.balance * self.int_rate, 2)
+        return self
 
 account1 = BankAccount(.03, 100)
 
@@ -32,3 +37,4 @@ account1.display_info()
 
 #Create a second account make 2 deposits 4 withdraws, then yield_interest and display_info using chaining 
 account2 = BankAccount(.03, 100)
+account2.deposit(100).deposit(10).withdraw(10).withdraw(1).withdraw(55).withdraw(10).yield_interest().display_info()
