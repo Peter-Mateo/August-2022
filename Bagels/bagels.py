a72bc8a77b8cb7a39cf3e4875df6d91a966dae2f
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-15 14:57:51 -0400

    Update bagels.py

--- Bagels/bagels.py
+++ Bagels/bagels.py
@@ -8,9 +8,28 @@ if intro_answer == "yes":
     clear()
     print("There are three digits that you must guess, with the given clues. You will be told when your guess has a correct digit in the correct place. You have 10 tries to guess the secret number.")
     ready_answer = input("Do you understand the rules?  ").lower()
-    if ready_answer == "yes":
-        logic_game_answer = rnd.randint(0,10), rnd.randint(0,10), rnd.randint(0,10)
-        print(logic_game_answer)
+    if ready_answer == "yes": ## Game starts 
+        clear()
+        logic_game_answer = str(rnd.randint(100,999))
+        # Initializing substring
+        A = 1 
+        #Creating a result list
+        result = []
+        for i in range(0, len(logic_game_answer), A):
+            #Converting to int, after the slicing process
+            result.append(int(logic_game_answer[i : i + A]))
+        print(result)
+        print("What is your first guess? ")
+        print("Don't forget it has to be three digits!")
+        guess = (int(input()),int(input()),int(input()))
+        if guess == logic_game_answer:
+            print("Your Guess was Spot on!!!")
+            print("You have Won!")
+            replay = input("Would you like to play again?  ").lower()
+            if replay == "yes":
+                pass
+        else:
+            pass
 else:
     clear()
     close = input("Would you like to close the program? Yes? No?  ").lower()
