db10a1a511ce90a67f0bc9679b7afce1e4c00236
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-15 03:52:11 -0400

    Update flower_oop.py

---
+++ flower_OOP/flower_oop.py
@@ -0,0 +1,7 @@
+class flower:
+    def __init__(self, color, type, been_picked = False, height, num_petals):
+        self.color = color
+        self.type = type
+        self.been_picked = been_picked
+        self.height = height
+        self.num_petals = num_petals
