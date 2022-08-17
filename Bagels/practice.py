79ff1cba486d9ac29398aa7ab30dff8f7879b85d
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-17 12:58:44 -0400

    Update practice.py

--- Bagels/practice.py
+++ Bagels/practice.py
@@ -8,19 +8,19 @@ Welcome to the deductive logic game, I will think of a three digit number in whi
 ready_q1 = input("Are you ready to play?    ").lower()
 if ready_q1 == 'yes':
     clear()
-    print("""
+    while True:
+        print("""
 The game offers one of the follwing hints in response to your guess: 
 When I say:         that means:
 Pico                Guess is the correct digit but in the wrong place
 Fermi               Correct digit in the correct place
 Bagels              All three digits aren't any of the numbers I was thinking of    
 """)
-    while True:
         understand_rules = input("Do you understand these rules? We will also leave the key at the top of the screen in case you forget them!")
         if understand_rules == 'yes':
             break
         else:
-            close = input("If you would like to close the game, you can by typing 'close' ")
+            close = input("If you would like to close the game, you can by typing 'close' ").lower()
             if close == 'close':
                 quit()
             else:
