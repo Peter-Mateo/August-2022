017703163db029175f2c915885b4e106f4ce2921
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-19 14:33:09 -0400

    Update loop.js

--- JS_loop_challenges/loop.js
+++ JS_loop_challenges/loop.js
@@ -1,22 +1,28 @@
 // Print odds 1-20
 for (var i = 1; i < 21; i++) {
     if (i % 2 == 0) {
-        console.log(i)
+        console.log(i);
     }
 }
 // Decreasing Multiples of 3 
 for (var i = 100; i > 0; i--){
     if (i % 3 == 0){
-        console.log(i)
+        console.log(i);
     }
 }
 // Print the sequence 
 for (var i = 4; i > -4; i -= 1.5){
-    console.log(i)
+    console.log(i);
 }
 // Sigma
 var sum = 0;
 for(var i = 1; i < 101; i++){
-    sum += i
+    sum += i;
 }
-console.log(sum)
+console.log(sum);
+// Factorial\
+var product = 1;
+for (var i = 2; i < 13; i++){
+    product *= i;
+}
+console.log(product);
