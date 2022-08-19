37a6c68abe7c353b3d81ae277c7e6e74d73d726e
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-19 14:22:26 -0400

    Update loop.js

--- JS_loop_challenges/loop.js
+++ JS_loop_challenges/loop.js
@@ -4,3 +4,9 @@ for (var i = 1; i < 21; i++) {
         console.log(i)
     }
 }
+// Decreasing Multiples of 3 
+for (var i = 100; i > 0; i--){
+    if (i % 3 == 0){
+        console.log(i)
+    }
+}
