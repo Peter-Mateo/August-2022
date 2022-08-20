6f12c6874ed423d48df22148488cbb80519c397d
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-20 13:38:50 -0400

    Update hello.py

--- Flask_web_development/hello.py
+++ Flask_web_development/hello.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Bad Request</h1>', 300
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
 @app.route('/new/<name>')
 def new(name):
