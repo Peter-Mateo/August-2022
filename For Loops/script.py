e8da92f7f66b6dbe4fd692f071aef7478b646465
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-20 00:37:33 -0400

    Update script.py

--- For Loops/script.py
+++ For Loops/script.py
@@ -1,17 +1,26 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for a in range(len(nums)):
-                if i == a:
-                    continue
-                check = nums[i] + nums[a]
-                if check == target:
-                    break
-        print(i, a)
-        return (i, a)
-ten = []
-for i in range(10_000):
-    ten.append(i)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+def iterate(list):
+    reverbed = ""
+    for i in reversed(range(len(list))):
+        reverbed += str(list[i])
+    return int(reverbed)
 
-two = Solution()
-two.twoSum([1,2,3,4,5,6,7,8,9,10], 19)
+def breaks(string):
+    one = string[:1] 
+    two = string[1::3]
+    three = string[2::]
+    return [int(three), int(two), int(one)]
+    
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        list_1 = iterate(l1)
+        list_2 = iterate(l2)
+        combo = str(list_1 + list_2)
+        return breaks(combo)
+
+new = Solution()
+print(new.addTwoNumbers([2,4,3], [5,6,4]))
