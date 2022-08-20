9b4fda97034d7c73b6939ceb0fae99688cac7e37
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-20 14:40:55 -0400

    Update app.py

--- myproject/app.py
+++ myproject/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 # View function
@@ -7,6 +7,17 @@ def index():
     return 'Index page'
 
 # View function
-@app.route('/hello/')
+@app.route('/login/')
 def hello():
-    return 'Hello, World'
+    return 'login'
+
+# View function
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next = '/'))
+    print(url_for('profile', next = '/'))
