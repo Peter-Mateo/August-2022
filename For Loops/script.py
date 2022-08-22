2b56dedeed584c7aa41370d83e73e51aac8d780d
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-22 00:42:22 -0400

    Update script.py

--- For Loops/script.py
+++ For Loops/script.py
@@ -1,26 +1,27 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-def iterate(list):
-    reverbed = ""
-    for i in reversed(range(len(list))):
-        reverbed += str(list[i])
-    return int(reverbed)
-
-def breaks(string):
-    one = string[:1] 
-    two = string[1::3]
-    three = string[2::]
-    return [int(three), int(two), int(one)]
-    
 class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        list_1 = iterate(l1)
-        list_2 = iterate(l2)
-        combo = str(list_1 + list_2)
-        return breaks(combo)
-
-new = Solution()
-print(new.addTwoNumbers([2,4,3], [5,6,4]))
+    def lengthOfLongestSubstring(self, s):
+        # Declaring the String
+        so_far = ''
+        # For loop to start 
+        for letter in s:
+            # Skip the first letter
+            if so_far == '':
+                so_far += letter
+            # Check if the letter is in the string 
+            if letter in so_far:
+                print(so_far)
+                return len(so_far)
+            # Add the letter to the new 
+            else:
+                print(so_far)
+                so_far += letter
+                
+                
+        # Create a for loop that loops through each letter in so_far to check against the current letter 
+        
+        """
+        :type s: str
+        :rtype: int
+        """
+test = Solution()
+test.lengthOfLongestSubstring('"abcabcbb"')
