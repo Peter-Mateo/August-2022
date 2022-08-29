ace433fbdb7f1faff5c8a3dd93b9bc4a142a616d
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-08-28 21:08:20 -0400

    finished

--- flask_playground/server.py
+++ flask_playground/server.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/play')
+def index():
+    return render_template('index.html')
+
+@app.route('/play/<times>')
+def play(times):
+    return render_template('next_step.html', times=int(times))
+
 @app.route('/play/<times>/<color>')
-def index(times, color):
-    return render_template('index.html', times = times, color = color)
+def play_color(times, color):
+    return render_template('last_step.html', times=int(times), color=color)
 
 if __name__ == '__main__':
     app.run(debug=True)
